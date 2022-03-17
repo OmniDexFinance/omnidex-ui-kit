d6da4ee68df0d54638f7e30d50e63a89623f190b
tomtomcrypto <titiomm@gmail.com>
2022-03-17 14:28:59 +0000

    make dark default theme

--- src/libs/theme-provider/themes.ts
+++ src/libs/theme-provider/themes.ts
@@ -2,28 +2,6 @@ import { Themes } from './types';
 
 // eslint-disable-next-line import/prefer-default-export
 export const themes: Themes = {
-  dark: {
-    headerBg: { hex: '#20202e', rgb: [32, 32, 46] },
-    mainBg: { hex: '#000000', rgb: [0, 0, 0] },
-    mainBgCenter: { hex: '#6a2e2e', rgb: [106, 46, 46] },
-    border: { hex: '#565055', rgb: [86, 80, 85] },
-    primary: { hex: '#e9981b', rgb: [233, 152, 27] },
-    secondary: { hex: '#e9981b', rgb: [233, 152, 27] },
-    darkBlue: { hex: '#181717', rgb: [24, 23, 23] },
-    textDarkBlue: { hex: '#f1f1f3', rgb: [241, 241, 243] },
-    lightBlue: { hex: '#8d8f99', rgb: [141, 143, 153] },
-    red: { hex: '#df3f42', rgb: [223, 63, 66] },
-    orange: { hex: '#ffac4d', rgb: [255, 172, 77] },
-    green: { hex: '#0b8f08', rgb: [11, 143, 8] },
-    white: { hex: '#f1f1f3', rgb: [241, 241, 243] },
-    whiteElement: { hex: '#181717', rgb: [56, 61, 81] },
-    whiteItem: { hex: '#372f47', rgb: [55, 47, 71] },
-    darkOrange: { hex: '#2ea828', rgb: [46, 168, 40] }, // for deposit APY
-    gray: { hex: '#707070', rgb: [112, 112, 112] },
-    lightGray: { hex: '#a2a2a2', rgb: [162, 162, 162] },
-    darkGray: { hex: '#565055', rgb: [86, 80, 85] },
-    disabledGray: { hex: '#20202e', rgb: [32, 32, 46] },
-  },
   default: {
     headerBg: { hex: '#2b2d3c', rgb: [43, 45, 60] },
     mainBg: { hex: '#f1f1f3', rgb: [241, 241, 243] },
@@ -45,5 +23,27 @@ export const themes: Themes = {
     lightGray: { hex: '#a2a2a2', rgb: [162, 162, 162] },
     darkGray: { hex: '#a2a2a2', rgb: [162, 162, 162] },
     disabledGray: { hex: '#e0e0e2', rgb: [224, 224, 226] },
+  },
+  dark: {
+    headerBg: { hex: '#20202e', rgb: [32, 32, 46] },
+    mainBg: { hex: '#000000', rgb: [0, 0, 0] },
+    mainBgCenter: { hex: '#6a2e2e', rgb: [106, 46, 46] },
+    border: { hex: '#565055', rgb: [86, 80, 85] },
+    primary: { hex: '#e9981b', rgb: [233, 152, 27] },
+    secondary: { hex: '#e9981b', rgb: [233, 152, 27] },
+    darkBlue: { hex: '#181717', rgb: [24, 23, 23] },
+    textDarkBlue: { hex: '#f1f1f3', rgb: [241, 241, 243] },
+    lightBlue: { hex: '#8d8f99', rgb: [141, 143, 153] },
+    red: { hex: '#df3f42', rgb: [223, 63, 66] },
+    orange: { hex: '#ffac4d', rgb: [255, 172, 77] },
+    green: { hex: '#0b8f08', rgb: [11, 143, 8] },
+    white: { hex: '#f1f1f3', rgb: [241, 241, 243] },
+    whiteElement: { hex: '#181717', rgb: [56, 61, 81] },
+    whiteItem: { hex: '#372f47', rgb: [55, 47, 71] },
+    darkOrange: { hex: '#2ea828', rgb: [46, 168, 40] }, // for deposit APY
+    gray: { hex: '#707070', rgb: [112, 112, 112] },
+    lightGray: { hex: '#a2a2a2', rgb: [162, 162, 162] },
+    darkGray: { hex: '#565055', rgb: [86, 80, 85] },
+    disabledGray: { hex: '#20202e', rgb: [32, 32, 46] },
   },
 };
